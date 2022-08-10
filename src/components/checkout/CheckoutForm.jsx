e61918f8250b4dd7c74d22ce39c205efa619d640
Richard Aiello <richaiello17@gmail.com>
2022-08-10 12:37:26 -0400

    Starting validation schema in CheckoutValidation.jsx.

--- src/components/checkout/CheckoutForm.jsx
+++ src/components/checkout/CheckoutForm.jsx
@@ -1,30 +1,87 @@
+import { useState } from 'react';
+import { useCartState } from '../../context/CartContext';
 import { Form, Field, ErrorMessage } from 'formik';
-import useCartState from '../../context/CartContext';
+import {
+  CountryDropdown,
+  RegionDropdown,
+  CountryRegionData,
+} from 'react-country-region-selector';
 import CheckoutValidation from './CheckoutValidation';
 
 const CheckoutForm = props => {
   const cart = useCartState();
+  const [cash, setCash] = useState(false);
 
   return (
     <CheckoutValidation>
       <Form className="form">
         <section className="form__wrapper">
-          <h1>Checkout</h1>
+          <h1 className="heading mb-8">Checkout</h1>
           <section>
-            <h2>Billing Details</h2>
-            <div></div>
+            <h2 className="form__heading--section">
+              Billing Details
+            </h2>
+            <div className="form__wrapper--secondary">
+              <div className="form__wrapper--field">
+                <label htmlFor="name" className="form__label">
+                  Name
+                </label>
+                <Field
+                  name="name"
+                  type="text"
+                  className="form__input"
+                  placeholder="Name"
+                />
+                <ErrorMessage name="name">
+                  {msg => <div className="form__error">{msg}</div>}
+                </ErrorMessage>
+              </div>
+              <div className="form__wrapper--field">
+                <label htmlFor="email" className="form__label">
+                  Email Address
+                </label>
+                <Field
+                  name="email"
+                  type="email"
+                  className="form__input"
+                  placeholder="youremail@mail.com"
+                />
+                <ErrorMessage name="email">
+                  {msg => <div className="form__error">{msg}</div>}
+                </ErrorMessage>
+              </div>
+              <div className="form__wrapper--field">
+                <label htmlFor="phone" className="form__label">
+                  Phone Number
+                </label>
+                <Field
+                  name="phone"
+                  type="text"
+                  className="form__input"
+                  placeholder="+1 202-555-0136"
+                />
+                <ErrorMessage name="phone">
+                  {msg => <div className="form__error">{msg}</div>}
+                </ErrorMessage>
+              </div>
+            </div>
           </section>
           <section>
-            <h2>Shipping Info</h2>
-            <div></div>
+            <h2 className="form__heading--section">Shipping Info</h2>
+            <div className="form__wrapper--secondary"></div>
           </section>
           <section>
-            <h2>Payment Details</h2>
-            <div></div>
+            <h2 className="form__heading--section">
+              Payment Details
+            </h2>
+            <div className="form__wrapper--secondary"></div>
           </section>
         </section>
         <section className="form__wrapper">
           <h3>Summary</h3>
+          <button type="submit" className="form__submit">
+            Submit
+          </button>
         </section>
       </Form>
     </CheckoutValidation>
--- src/components/checkout/CheckoutValidation.jsx
+++ src/components/checkout/CheckoutValidation.jsx
@@ -6,26 +6,56 @@ const CheckoutValidation = ({ children }) => {
   return (
     <Formik
       initialValues={{
-        fullName: '',
+        name: '',
         email: '',
         phone: '',
-        message: '',
+        address: '',
+        zip: '',
+        city: '',
+        country: '',
+        regionState: '',
+        payment: 'eMoney',
+        eMoneyNumber: '',
+        eMoneyPIN: '',
       }}
       validationSchema={Yup.object({
-        fullName: Yup.string()
+        name: Yup.string()
           .matches(/^[a-z ,.'-]+$/i, 'Letters only')
-          .max(20, 'Must be 20 characters or less')
-          .required('Required'),
+          .max(20, 'Must be 20 characters or less.')
+          .required('Required!'),
         email: Yup.string()
           .email('Invalid email')
-          .required('Required'),
+          .required('Required!'),
+        // Set a condition chekcing if phone number matches the selected country
         phone: Yup.string()
-          .required('Required')
-          .phone('US', null, 'Invalid 10 digit US phone number'),
-        message: Yup.string().max(
-          200,
-          'Must be 200 characters or less.'
-        ),
+          .required('Required!')
+          .phone('US', 'Please enter a valid phone number.'),
+        address: Yup.string()
+          .max(25, 'Must be 25 characters or less.')
+          .required('Required!'),
+        zip: Yup.number()
+          .positive()
+          .integer()
+          .min(5, 'Enter a 5 digit zip code.')
+          .max(5, 'Enter a 5 digit zip code.')
+          .required('Required!'),
+        city: Yup.string()
+          .max(15, 'Max 15 characters.')
+          .required('Required!'),
+        country: Yup.string().required('Required!'),
+        regionState: Yup.string().required('Required!'),
+        eMoneyNumber: Yup.number()
+          .positive()
+          .integer()
+          .min(9, 'Enter a 9 digit e-Money number.')
+          .max(9, 'Enter a 9 digit e-Money number.')
+          .required('Required!'),
+        eMoneyPIN: Yup.number()
+          .positive()
+          .integer()
+          .min(4, 'Enter a 4 digit e-Money PIN.')
+          .max(4, 'Enter a 4 digit e-Money PIN.')
+          .required('Required!'),
       })}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
