37b79649d176dd18e807676e84e049798a700a49
Richard Aiello <richaiello17@gmail.com>
2022-08-12 14:43:35 -0400

    Styles for form on tablet.

--- src/components/checkout/CustomRadio.jsx
+++ src/components/checkout/CustomRadio.jsx
@@ -1,15 +1,20 @@
 import { Field } from 'formik';
 
-const CustomRadio = ({ handleChange, setState, state }) => {
+const CustomRadio = ({
+  handleChange,
+  setState,
+  state,
+  className,
+}) => {
   return (
-    <div className="form__wrapper--radio">
+    <div className="form__wrapper--radio md:col-span-2 md:grid md:grid-cols-2 md:gap-x-4">
       <div id="radio-group" className="form__label">
         Payment Methods
       </div>
       <div
         role="group"
         aria-labelledby="radio-group"
-        className="form__radio"
+        className={`form__radio ${className}`}
       >
         <label className="form__input--radio">
           <Field
