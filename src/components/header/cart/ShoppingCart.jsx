e95f7580d68d0a38257b9f972c1ed001116699a4
Richard Aiello <richaiello17@gmail.com>
2022-08-12 13:32:03 -0400

    Styles for cart on tablet.

--- src/components/header/cart/ShoppingCart.jsx
+++ src/components/header/cart/ShoppingCart.jsx
@@ -9,19 +9,21 @@ const ShoppingCart = ({ className, onClick }) => {
   const cart = useCartState();
 
   return (
-    <div
-      className={`
-        container absolute z-10 top-[7.125rem] left-0 right-0 mx-auto rounded-lg 
-        max-h-[calc(100vh-8.625rem)] overflow-y-scroll bg-white py-8 px-7
-        grid grid-cols-[1fr,1fr]
-        ${className}
-      `}
-    >
-      <CartCount cart={cart} />
-      <EmptyCartBtn />
-      <CartContents cart={cart} />
-      <CartTotal cart={cart} />
-      <CheckoutBtn onClick={onClick} />
+    <div className="container absolute z-10 top-[7.125rem] left-0 right-0 h-max">
+      <div
+        className={`
+          w-[min(100%,380px)] rounded-lg ml-auto
+          max-h-[calc(100vh-8.625rem)] overflow-y-scroll bg-white py-8 px-7
+          grid grid-cols-[1fr,1fr]
+          ${className}
+        `}
+      >
+        <CartCount cart={cart} />
+        <EmptyCartBtn />
+        <CartContents cart={cart} />
+        <CartTotal cart={cart} />
+        <CheckoutBtn onClick={onClick} />
+      </div>
     </div>
   );
 };
