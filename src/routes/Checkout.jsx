ffab988c0b2fb13add17d4a6424ef3d09f668eab
Richard Aiello <richaiello17@gmail.com>
2022-08-12 09:53:29 -0400

    Conditionally rendered CheckoutForm.jsx based on items in cart.

--- src/routes/Checkout.jsx
+++ src/routes/Checkout.jsx
@@ -1,12 +1,21 @@
+import { useCartState } from '../context/CartContext';
 import BackBtn from '../components/shared-components/BackBtn';
 import CheckoutForm from '../components/checkout/CheckoutForm';
 import '../styles/form.css';
 
 const Checkout = props => {
+  const cart = useCartState();
+
   return (
     <main className="container mt-[6.625rem]">
       <BackBtn />
-      <CheckoutForm />
+      {cart.length === 0 ? (
+        <p>
+          Please add items to your cart before visiting this page.
+        </p>
+      ) : (
+        <CheckoutForm />
+      )}
     </main>
   );
 };
