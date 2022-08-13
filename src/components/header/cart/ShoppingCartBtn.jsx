78cf2eb5c1393738cf5ddb96096d936da9b9f507
Richard Aiello <richaiello17@gmail.com>
2022-08-13 13:12:39 -0400

    Added icon to cart btn to display cart quantity when not viewing cart.

--- src/components/header/cart/ShoppingCartBtn.jsx
+++ src/components/header/cart/ShoppingCartBtn.jsx
@@ -1,16 +1,26 @@
+import { useCartState } from '../../../context/CartContext';
 import { ReactComponent as Icon } from '../../../assets/cart.svg';
 
 const ShoppingCartBtn = props => {
+  const cart = useCartState();
+
   return (
     <button
       type="button"
       aria-label="Open shopping cart."
-      className={`cursor-pointer md:ml-auto
+      className={`cursor-pointer md:ml-auto flex items-center
         ${props.className}
       `}
       onClick={props.onClick}
       disabled={props.disabled}
     >
+      {cart.length > 0 && (
+        <span className="font-bold mr-2 bg-light-orange px-2 rounded-full">
+          {cart.reduce((total, current) => {
+            return total + current.quantity;
+          }, 0)}
+        </span>
+      )}
       <Icon className="icon-cart" />
     </button>
   );
